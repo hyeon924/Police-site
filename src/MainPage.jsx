b25eeaf5e35c31a2355a4f277f8dba9e6d2193eb
hyeon924 <hyeonn4713@gmail.com>
2025-04-12 01:37:26 +0900

    main page 추가 및 scss 적용

---
+++ src/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import './styles/MainPage.scss';
+
+const MainPage = () => {
+    return (
+        <div className="main-page">
+            <h1>main</h1>
+        </div>
+    );
+};
+
+export default MainPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+// 전역 스타일 import (공통 변수, 리셋 등)
+import './styles/global.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
